d3771b663d749cc3ecdf3b3f9478bbefdb9e079a
Dev.Park <firoful@gmail.com>
2021-08-04 22:58:13 +0900

    delegate exam

---
+++ unityextensionui/Assets/Script/delegate2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delegate2 : MonoBehaviour
+{
+    public deleteGateTest degate = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        degate.anLove += sadHeart;
+
+        degate.setDelegate();
+
+        degate.anLove(3000);
+    }
+
+    void sadHeart(int value)
+    {
+        Debug.Log("i am nobody without you : "+value);
+    }
+}
--- unityextensionui/Assets/Script/deleteGateTest.cs
+++ unityextensionui/Assets/Script/deleteGateTest.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class deleteGateTest : MonoBehaviour
 {
+    public Action<int> anLove;
+
+    public delegate void myDream(int value);
+    public myDream fallenDream;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setDelegate()
     {
-        
+        anLove += addMyHeart;
+        anLove += addYourHeart;
+
+        fallenDream += addMyHeart;
+        fallenDream += addYourHeart;
+
+        anLove(5);
+
+        fallenDream(10);
+    }
+
+    public void addMyHeart(int value)
+    {
+        Debug.Log("my heart : "+value);
+    }
+
+    public void addYourHeart(int value)
+    {
+        Debug.Log("your heart : "+value);
     }
 }
